eb37bd089937fee21c76bde49102fcca4aa516e8
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-11-12 11:39:38 -0500

    next/config loader added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
     reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
     webpack5: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false }

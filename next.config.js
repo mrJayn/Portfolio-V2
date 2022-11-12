0ef9499d0afef20e618476faa21287e47ac57339
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-11-12 18:10:15 -0500

    More NextConfig conditionals added

--- next.config.js
+++ next.config.js
@@ -2,16 +2,29 @@
 
 const prod = process.env.NODE_ENV === 'production'
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
 module.exports = {
     env: {
-        BACKEND_URL: prod ? '/Portfolio' : '/',
+        BACKEND_URL: prod ? '/Portfolio' : '',
     },
     images: {
         loader: 'akamai',
         path: '',
     },
-    basePath: prod ? '/Portfolio' : '/',
-    assetPrefix: prod ? '/Portfolio' : '',
+    basePath: prod ? basePath : '',
+    assetPrefix: prod ? assetPrefix : '',
     webpack5: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false }

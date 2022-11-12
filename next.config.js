116161390695f2e962a8a9100eb05843c45a1f2f
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-11-12 14:03:39 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
 module.exports = {
     reactStrictMode: true,
+
     images: {
         loader: 'akamai',
         path: '',
     },
     basePath: '/Portfolio',
-    assetPrefix: '/Portfolio',
+    assetPrefix: '/Portfolio/',
     webpack5: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false }
-
         return config
     },
 }

fb66edec7ab479ee93d4feca55b5422141eb1f60
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-11-12 17:58:22 -0500

    env vars to nextConfig and "if prod..." added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const prod = process.env.NODE_ENV === 'production'
+
 module.exports = {
+    env: {
+        BACKEND_URL: prod ? '/Portfolio' : '/',
+    },
     images: {
         loader: 'akamai',
         path: '',
     },
-    basePath: '/Portfolio',
-    assetPrefix: '/Portfolio',
+    basePath: prod ? '/Portfolio' : '/',
+    assetPrefix: prod ? '/Portfolio' : '',
     webpack5: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false }

f277106b9f2eed250538b92cc5666eb6a4622ab4
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-11-22 13:19:24 -0500

    assetPrefix removed from nextConfig

--- next.config.js
+++ next.config.js
@@ -11,8 +11,6 @@ let basePath = ''
 if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-    assetPrefix = `/${repo}/`
 }
 
 module.exports = {

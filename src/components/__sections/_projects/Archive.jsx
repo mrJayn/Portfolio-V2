d15c40f0ffc922238f3367592de5ec24646fe261
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-12-16 19:13:30 -0500

    big update

--- src/components/__sections/_projects/Archive.jsx
+++ src/components/__sections/_projects/Archive.jsx
@@ -3,36 +3,30 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 import Project_Card from './Project_Cards'
 import { Tabs_List } from '@components'
-import { projectCardVariants } from '@motion'
-const variants = projectCardVariants.Archive
+import { archiveVariants as variants } from '@motion'
 
 const Header = () => {
     const vars = variants.Header
     return (
         <motion.div
-            className="flex-col-center my-6 gap-y-2 overflow-hidden text-center"
+            className="flex-col-center my-6 w-full text-center lg:my-0"
             initial="hidden"
             whileInView="show"
             viewport={{ once: true }}
             variants={vars.container}
         >
             <motion.h3 className="relative" variants={vars.items}>
-                <motion.span
-                    className="styled-underline -hue-rotate-60"
-                    variants={vars.decoration}
-                />
                 Projects Archive
             </motion.h3>
-            <motion.p variants={vars.items}>
+            <motion.h4 className="text-black" variants={vars.items}>
                 Other noteworthy creations of mine
-            </motion.p>
+            </motion.h4>
         </motion.div>
     )
 }
 
-const Archive = ({ isMd, pRM, ...sectionData }) => {
+const Archive = ({ isSm, isMd, isLg, ...sectionData }) => {
     const [[currentTab, _], setTab] = useState([0, 0])
-    const [activeProject, setActiveProject] = useState(-1)
 
     const projects = Object.values(sectionData)
     const projectCategories = projects.map((project) => project.data.category)
@@ -52,47 +46,53 @@ const Archive = ({ isMd, pRM, ...sectionData }) => {
     }
 
     function handleSelect(clickedTab) {
+        document
+            .getElementById('archive-top')
+            .scrollIntoView({ behavior: 'smooth' })
+
         if (clickedTab == currentTab) return
         let newDirection = clickedTab - currentTab
         setTab([clickedTab, newDirection])
     }
 
-    const DisplayCards = groupedTabs[currentTab]
-    const Blanks = [...Array(projects.length - DisplayCards.length).keys()]
+    const ActiveProjects = groupedTabs[currentTab]
+
+    const [projectCardHeight, gridCols] = isMd ? [50, 2] : [40, 1]
+
+    const usedSpace =
+        Math.round(ActiveProjects.length / gridCols) * projectCardHeight
+    // over 2 because grid is using two columns
 
     const ProjectsTabWrap = () => (
         <>
-            {DisplayCards.map((project, i) => (
+            {ActiveProjects.map((projectData, i) => (
                 <motion.div
                     key={`grouped-projects-${i}`}
-                    className="relative h-[150px] overflow-hidden rounded-xl sm:h-[225px] sm:shadow-xs md:h-[300px] md:cursor-default lg:h-full"
+                    className="relative h-[40vh] overflow-hidden transition-[filter] duration-250 ease-tween hover:brightness-110 md:h-[50vh]"
                     variants={variants.ProjectCard}
                 >
-                    <Project_Card
-                        project={project}
-                        i={i}
-                        activeProject={activeProject}
-                        setActiveProject={setActiveProject}
-                        isMd={isMd}
-                    />
+                    <Project_Card data={projectData.data} i={i} isMd={isMd} />
                 </motion.div>
             ))}
-            {Blanks.map((i) => (
-                <div
-                    key={`blankCard-${i}`}
-                    className="pointer-events-none relative h-[225px] md:h-[300px]"
-                />
-            ))}
+            <div
+                className="full pointer-events-none relative border-2 md:col-span-2"
+                style={{
+                    height: isMd
+                        ? `calc(${75 - usedSpace}vh - 56px)`
+                        : `calc(75vh - ${usedSpace}px)`,
+                }}
+            />
         </>
     )
 
     return (
-        <div className="flex-col-center w-full">
-            {/** Title **/}
-            {isMd ? <Header /> : <h3>Archive</h3>}
+        <div className="flex-col-center mt-12 w-full">
+            <a id="archive-top" className="scroll-mt-14" />
 
-            <div className="relative h-auto w-full pt-12">
-                <div className="sticky top-14 left-0 z-10 h-12 w-full md:absolute md:top-0">
+            {isLg ? <Header /> : <h3>Archive</h3>}
+
+            <div className="relative w-full md:mt-12">
+                <div className="sticky inset-x-0 top-14 z-10 h-14 pb-2 lg:absolute lg:top-0">
                     <Tabs_List
                         currentTab={currentTab}
                         tabNames={tabNames}
@@ -100,26 +100,16 @@ const Archive = ({ isMd, pRM, ...sectionData }) => {
                     />
                 </div>
                 <div className="full">
-                    <AnimatePresence
-                        mode="wait"
-                        onExitComplete={() => setActiveProject(-1)}
-                    >
+                    <AnimatePresence mode="wait">
                         <motion.div
                             key={currentTab}
-                            className="mt-4 grid grid-cols-1 gap-2 sm:grid-cols-2 md:gap-4 lg:grid-cols-3"
+                            className="grid grid-cols-1 md:grid-cols-2 md:gap-0"
                             initial="hidden"
                             animate="show"
                             exit="exit"
                             variants={variants.Wrap}
-                            custom={pRM}
                         >
-                            <ProjectsTabWrap
-                                DisplayCards={DisplayCards}
-                                Blanks={Blanks}
-                                activeProject={activeProject}
-                                setActiveProject={setActiveProject}
-                                isMd={isMd}
-                            />
+                            <ProjectsTabWrap />
                         </motion.div>
                     </AnimatePresence>
                 </div>

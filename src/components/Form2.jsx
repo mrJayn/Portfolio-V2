d8d40104bb9e0511596e19f7cd68a3e3d73b7c8d
mrJayn <100537512+mrJayn@users.noreply.github.com>
2023-06-21 17:40:32 -0400

    New Form basic styling

--- src/components/Form2.jsx
+++ src/components/Form2.jsx
@@ -2,30 +2,98 @@ import React from 'react'
 import { useForm, ValidationError } from '@formspree/react'
 import { StyledBtn } from '@components'
 
-const ToastMsg = ({ success }) => (
-    <div className="py-2 text-center font-raleway text-black">
-        <div className="mb-[0.5em] text-[1.125em] font-semibold">{success ? 'Thank you!' : 'Uh Oh...'}</div>
-        <div className="text-min leading-[1.125]">
-            {success
-                ? `I've recieved your message,\n and will get back to you ASAP!`
-                : `Oops! Something wasn't quite right. Please try again!`}
-        </div>
-    </div>
-)
+const emailPattern =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const inputs = {
+    name: {
+        text: 'Name*',
+        error: 'Enter your name.',
+    },
+    email: {
+        text: 'Email Address*',
+        error: "Enter a valid or alternate email address.'",
+    },
+    subject: {
+        text: 'Subject*',
+        error: "Enter a subject.'",
+    },
+    message: {
+        text: 'Message*',
+        error: 'Enter a message.',
+    },
+}
+
+const itemProps = (key) => ({
+    className: `peer z-0 w-full rounded-md border-2 border-t-0 border-grey-40 bg-[#e0e0e0] pl-[5px] leading-[1] text-black outline-none transition-colors focus:border-slate-neon ${
+        key === 'message' ? 'resize-none overflow-y-scroll pt-[calc(1em)]' : 'h-[40px] pt-[calc(40px-1.5em)] lg:h-[55px]'
+    }`,
+    name: key,
+    id: key,
+    placeholder: ' ',
+    'data-text': inputs[key].text,
+    required: true,
+})
 
 export default function Form() {
     const [state, handleSubmit] = useForm('meqwybzy')
     if (state.succeeded) {
-        return <p>Thanks for joining!</p>
+        return <p>Thanks for the message!</p>
+    }
+
+    const inputs = {
+        name: {
+            text: 'Name*',
+            component: <input type="text" autoComplete="name" {...itemProps('name')} />,
+            error: 'Enter your name.',
+        },
+        email: {
+            text: 'Email Address*',
+            component: <input type="email" autoComplete="email" {...itemProps('email')} />,
+            error: "Enter a valid or alternate email address.'",
+        },
+        subject: {
+            text: 'Subject*',
+            component: <input type="text" {...itemProps('subject')} />,
+            error: "Enter a subject.'",
+        },
+        message: {
+            text: 'Message*',
+            component: (
+                <textarea
+                    type="text"
+                    autoFocus={false}
+                    rows={10}
+                    defaultValue={''}
+                    maxLength={1000}
+                    {...itemProps('message')}
+                />
+            ),
+            error: 'Enter a message.',
+        },
     }
     return (
         <form className="flex-col-center w-full" onSubmit={handleSubmit}>
-            <label htmlFor="email">Email Address</label>
-            <input id="email" type="email" name="email" />
-            <ValidationError prefix="Email" field="email" errors={state.errors} />
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" />
-            <ValidationError prefix="Message" field="message" errors={state.errors} />
+            {Object.entries(inputs).map(([key, { text, component }]) => (
+                <div key={text} className="group relative w-full">
+                    {component}
+                    <ReactivePlaceholder>{text}</ReactivePlaceholder>
+                    <ErrorMessage error={false}>{inputs[key].error}</ErrorMessage>
+                    <ValidationError field={key} errors={state.errors} />
+                </div>
+            ))}
+
+            {/* 
+            <input type="text" autoComplete="name" {...itemProps('name')} />
+            <ValidationError field="name" errors={state.errors} />
+            <input type="email" autoComplete="email" {...itemProps('email')} />
+            <ValidationError field="email" errors={state.errors} />
+            <input type="text" {...itemProps('subject')} />
+            <ValidationError field="subject" errors={state.errors} />
+            <textarea type="text" autoFocus={false} rows={10} defaultValue={''} maxLength={1000} {...itemProps('message')} />
+            <ValidationError field="message" errors={state.errors} />
+            */}
+
             <StyledBtn type="submit" disabled={state.submitting}>
                 SUBMIT
             </StyledBtn>

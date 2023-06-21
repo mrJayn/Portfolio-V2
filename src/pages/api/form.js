67d1d3b15267bf8e8e2034fcc44a9a1813c4084f
mrJayn <100537512+mrJayn@users.noreply.github.com>
2023-06-21 10:42:56 -0400

    Nodemailer Fix 2

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -5,14 +5,15 @@ export default function handler(req, res) {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'site.email.notification@gmail.com',
-            pass: process.env.PASSWORD,
+            user: process.env.SITE_EMAIL_USERNAME,
+            pass: process.env.SITE_EMAIL_PASSWORD,
         },
         secure: true,
     })
+
     const mailData = {
-        from: process.env.EMAIL,
-        to: 'm63jayne@gmail.com',
+        from: process.env.SITE_EMAIL_USERNAME,
+        to: process.env.EMAIL_USERNAME,
         subject: `${req.body.subject}`,
         text: req.body.message,
         html: `
@@ -22,14 +23,24 @@ export default function handler(req, res) {
     <hr/>
     <br/>
     <div>${req.body.message}</div>`,
-    }``
+    }
 
     new Promise((resolve, reject) => {
-        transporter.sendMail(mailData, function (err, info) {
+        transporter.sendMail(mailData, function (error, res) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve('email sent')
+            }
+        })
+    })
+
+    /*
+       transporter.sendMail(mailData, function (err, info) {
             if (err) console.log('err--', err)
             else console.log('Message sent: ' + info)
         })
-    })
+    */
 
     res.status(200).end()
 }
